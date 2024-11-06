891991847cdf1df4f9200169320c1b3e9fba0153
melvinwaluyo <melvinwaluyo@mail.ugm.ac.id>
2024-11-06 12:25:13 +0700

    feat: Add Admin role, Add delete barang

--- server/src/models/userModels.js
+++ server/src/models/userModels.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
             unique: true,
+        },
+        role: 
+        {   type: String, 
+            enum: ['user', 'admin'], 
+            default: 'user' 
         }
     },
     {
