a7ba2e3e508c1140c479c76ae1837e0a65be0f7b
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2024-11-12 11:04:44 +0700

    feat: add state logged in to navbar

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Search, Menu, X } from 'lucide-react';
+import { Search, Menu, X, Plus } from 'lucide-react';
 import Image from 'next/image';
 import { Button } from './button';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to manage login status
 
   return (
     <nav className="bg-white px-4rem lg:px-[6rem] xl:px-[10rem] shadow-md border-b-2 border-gray-200">
@@ -33,16 +34,31 @@ const Navbar = () => {
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center space-x-4">
-            <a href="/" target="_blank" rel="noopener noreferrer" >
+            {isLoggedIn ? (
+              <a href="/" target='_blank' rel="noopener noreferrer">
+                <Button variant="outline" className="px-4 py-2 rounded-md">
+                  <Plus/>
+                </Button>
+              </a>
+            ): null}
+            <a href="/" target="_blank" rel="noopener noreferrer">
               <Button variant="outline" className="px-4 py-2 rounded-md">
-              Lapor Barang Temuan
+                Lapor Barang Temuan
               </Button>
             </a>
-            <a href="/" target="_blank" rel="noopener noreferrer" >
-              <Button variant="default" className="px-4 py-2 rounded-md">
-                Masuk Sekarang
-              </Button>
-            </a>
+            {isLoggedIn ? (
+              <a href="/profile" target="_blank" rel="noopener noreferrer">
+                <Button variant="default" className="px-4 py-2 rounded-md">
+                  Keluar
+                </Button>
+              </a>
+            ) : (
+              <a href="/" target="_blank" rel="noopener noreferrer">
+                <Button variant="default" className="px-4 py-2 rounded-md">
+                  Masuk Sekarang
+                </Button>
+              </a>
+            )}
           </div>
 
           {/* Mobile menu button */}
@@ -76,13 +92,19 @@ const Navbar = () => {
                 className="block w-full pl-10 pr-3 py-2 border border-darkBlue1 bg-red-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               />
             </div>
-            
+
             <button className="w-full text-blue-600 hover:bg-blue-50 px-4 py-2 rounded-lg border border-blue-600 mb-2">
               Lapor Barang Temuan
             </button>
-            <button className="w-full bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
-              Masuk Sekarang
-            </button>
+            {isLoggedIn ? (
+              <button className="w-full bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+                Profile
+              </button>
+            ) : (
+              <button className="w-full bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+                Masuk Sekarang
+              </button>
+            )}
           </div>
         </div>
       )}
